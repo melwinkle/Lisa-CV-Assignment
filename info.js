7bd72a548943c050ffb02e48d721cdb028cf3ad1
Aileen Akpalu <adzo.lisa@gmail.com>
2020-09-22 22:53:43 +0000

    update

--- info.js
+++ info.js
@@ -1,111 +1,148 @@
 
- 
-    let first=document.getElementById("f_name").value;
-    let middle=document.getElementById("m_name").value;
-    let last=document.getElementById("l_name").value;
-    let phone=document.getElementById("mynum").value;
-    let mail=document.getElementById("mymail").value;
-    let postal=document.getElementById("myadd").value;
-    let form=document.getElementById("form-info");
-
-function finaldata(){
-   
-    validate();
-    passvalue();
-
-}
- 
+    function handleSubmit(){
+        passvalue();
+    }
 
 
-function validate(){
-    if (valid_fname()) {
-		
-    if (valid_mname() ){
-    if (valid_lname()) {
-		
-    if (valid_number()) {
     
-    if (valid_post()) {
-    if (valid_mail()) {
-    }}}}}
-    }
-    return true; 
-}
+
+    function checkNumber(){
+        let num_len=/0-9a-zA-Z ]/;
+        let phonenum=document.getElementById("mynum");
+        if(!phonenum.value.match(num_len)){
+            document.getElementById("number_em").style.display = "inline";
+            return true;
+        }
+    };
+
+        function BlurPhone(){
+            let num_len=/0-9a-zA-Z ]/;
+            let phonenum=document.getElementById("mynum");
+            if(phonenum.value.match(num_len)){
+                document.getElementById("number_em").style.display = "none";
+            return false;
+        }
+    };
 
 
- /*
-Function to set the values in the form and store them locally
-*/
+    function checkFName(){
+        let name_len=/^[a-zA-Z\s-]+$/;
+        let fname= document.getElementById("f_name");
+        if(!fname.value.match(name_len)){
+            document.getElementById("First_em").style.display = "inline";
+            return true;
+        } 
+    };
+    function BlurFName(){   
+        let name_len=/^[a-zA-Z\s-]+$/;
+        let fname= document.getElementById("f_name");
 
+        if(fname.value.match(name_len)){
+            document.getElementById("First_em").style.display = "none";
+            return false;
+        }
+    
+};
+
+    function checkMName(){
+        let name_len=/^[a-zA-Z\s-]+$/;
+        let mname= document.getElementById("m_name");
+            if(!mname.value.match(name_len)){
+                document.getElementById("Middle_em").style.display = "inline";
+                return true;
+            }
+        };
+        function BlurMName(){
+            let name_len=/^[a-zA-Z\s-]+$/;
+            let mname= document.getElementById("m_name");
+                if(mname.value.match(name_len)){
+                    document.getElementById("Middle_em").style.display = "none";
+                return false;
+            }
+        };
+
+    function checkLName(){
+        let name_len=/^[a-zA-Z\s-]+$/;
+        let lname= document.getElementById("l_name");
+        if(!lname.value.match(name_len)){
+            lname.className=lname.className + "field-red";
+            document.getElementById("Surname_em").style.display = "inline";
+            return;
+        }
+    };
+
+    function BlurLName(){
+        let name_len=/^[a-zA-Z\s-]+$/;
+        let lname= document.getElementById("l_name");
+        if(lname.value.match(name_len)){
+            lname.className=lname.className.replace("field-red","");
+            document.getElementById("Surname_em").style.display = "none";
+            return;     
+                }
+               
+    };
+
+
+    function CheckAddress(){
+        let addr_len=/^[0-9a-zA-Z]+$/;
+        let address=document.getElementById("myadd");
+        if(!address.value.match(addr_len)){
+            document.getElementById("address_em").style.display = "inline";
+            return true;
+        }
+    };
+
+    function BlurAddress(){
+        let addr_len=/^[0-9a-zA-Z]+$/;
+        let address=document.getElementById("myadd");
+          if(address.value.match(addr_len)){
+            document.getElementById("address_em").style.display = "none";
+            return;
+          }
+        };
    
-function passvalue(){
-    let fname=document.getElementById("f_name").value;
-    let mname=document.getElementById("m_name").value;
-    let lname=document.getElementById("l_name").value;
-    let address=document.getElementById("myadd").value;
-    let mygen=document.getElementById("gender").value;
-    let email=document.getElementById("mymail").value;
-    let num=document.getElementById("mynum").value;
 
-
-localStorage.setItem("first",fname);
-localStorage.setItem("middle",mname);
-localStorage.setItem("last",lname);
-localStorage.setItem("gender",mygen);
-localStorage.setItem("address",address);
-localStorage.setItem("number",num);
-localStorage.setItem("email",email);
-
-return false;
-
-}
-
-
-
-function valid_mail(){
-    let pat_mail=/([a-z])([.]?[a-z])*@(ashesi.edu.gh)$/;
-    if(mail.match(pat_mail)){
-        return true;
-    }
-    else{
-        document.getElementById("mail_em").style.display = "inline";
+    function checkEmail(){
+        let mail_len=/([a-z])([.]?[a-z])*@(ashesi.edu.gh)$/;;
+        let mail=document.getElementById("mymail");
+        if(!mail.value.match(mail_len)){
+            document.getElementById("mail_em").style.display = "inline";
+            return true;
+        }
+    };
+        function BlurMail(){
+            let mail=document.getElementById("mymail");
+            let mail_len=/([a-z])([.]?[a-z])*@(ashesi.edu.gh)$/;;
+            if(mail.value.match(mail_len)){
+            document.getElementById("mail_em").style.display = "none";
         return false;
-    }
-}
-
-function valid_post(){
-    let pat_post=/0-9a-zA-Z ]/;
-    if(postal.match(pat_post)){
-        return true;
+            
+        }
     }
 
-}
-function valid_number(){
-    let pat_num= /^([0-9]{0,2})[0-9]/;
-    if(phone.match(pat_num)){
-        return true;
-    }
-}
-function valid_fname(){
-    let pat_name=/a-zA-z\s-/;
-    if(first.match(pat_name)){
-        return true;
-    }
-}
-function valid_mname(){
-    let pat_name=/a-zA-z\s-/;
-    if(middle.match(pat_name)){
-        return true;
-    }
-}
-function valid_lname(){
-    let pat_name=/a-zA-z\s-/;
-    if(last.match(pat_name)){
-        return true;
-    }
-}
-    
+
+
    
-        
-        
+    function passvalue(){
+        let fname=document.getElementById("f_name").value;
+        let mname=document.getElementById("m_name").value;
+        let lname=document.getElementById("l_name").value;
+        let address=document.getElementById("myadd").value;
+        let mygen=document.getElementById("gender").value;
+        let email=document.getElementById("mymail").value;
+        let num=document.getElementById("mynum").value;
+
+
+    localStorage.setItem("first",fname);
+    localStorage.setItem("middle",mname);
+    localStorage.setItem("last",lname);
+    localStorage.setItem("gender",mygen);
+    localStorage.setItem("address",address);
+    localStorage.setItem("number",num);
+    localStorage.setItem("email",email);
     
+    return false;
+
+}
+
+
