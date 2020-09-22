884f469994a00d92c860333f39642085228acb03
Aileen Akpalu <adzo.lisa@gmail.com>
2020-09-22 20:18:45 +0000

    updated

--- info.js
+++ info.js
@@ -31,11 +31,13 @@ Function to check and validate the data inputs
 */
 
 
-function validate(){
-    let pattern=/a-zA-z\s-/;
+//function validate(){
+
+
+    
     let number=/^([0-9]{0,2})[0-9]/;
     let email=/([a-z])([.]?[a-z])*@(ashesi.edu.gh)$/;
-    let address=/0-9a-zA-Z ]/;
+    let addres=/0-9a-zA-Z ]/;
     let first=document.getElementById("f_name").value;
     let middle=document.getElementById("m_name").value;
     let last=document.getElementById("l_name").value;
@@ -43,58 +45,75 @@ function validate(){
     let mail=document.getElementById("mymail").value;
     let postal=document.getElementById("myadd").value;
 
+function validatefName(){
+    let pattern=/a-zA-z\s-/;
+    if(first.match(pattern)){
+        document.getElementById("First_em").style.display = "none";
+        return true;
+    }
+    else{
+        document.getElementById("First_em").style.display = "inline";
+        return false;
+        }
+    
+    }
 
-    // if(!first.match(pattern)){
-    //     document.getElementById("First_em").style.display = "inline";
-    //     return false;
-    // }
-    if(!middle.match(pattern)){
+function validatemName(){
+    let pattern=/a-zA-z\s-/;
+    if(middle.match(pattern)){
+        document.getElementById("Middle_em").style.display = "none";
+        return true;
+    }
+    else{
         document.getElementById("Middle_em").style.display = "inline";
         return false;
     }
-    if(!last.match(pattern)){
+}
+
+function validatelName(){
+    let pattern=/a-zA-z\s-/;
+    if(last.match(pattern)){
+        document.getElementById("Last_em").style.display = "none";
+        return true;
+    }
+    else{
         document.getElementById("Last_em").style.display = "inline";
         return false;
     }
-
-    if(!phone.match(number)){
-        document.getElementById("number_em").style.display = "inline";
-        return false;
-    }
-    if(!mail.match(email)){
-        document.getElementById("mail_em").style.display = "inline";
-        return false;
-    }
-    if(!postal.match(address)){
-        document.getElementById("address_em").style.display = "inline";
-        return false;
-    }
-
-    else{
-        // if(first.match(patten)){
-        // document.getElementById("First_em").style.display = "none";
-        // return true;
-        // }
-        if(middle.match(patten)){
-            document.getElementById("Middle_em").style.display = "none";
-            return true;
-        }
-        if(last.match(patten)){
-            document.getElementById("Last_em").style.display = "none";
-            return true;
-        }
-        if(phone.match(number)){
-            document.getElementById("number_em").style.display = "none";
-            return true;
-        }
-        if(mail.match(email)){
-            document.getElementById("mail_em").style.display = "none";
-            return true;
-        }
-        if(postal.match(address)){
-            document.getElementById("address_em").style.display = "none";
-            return true;
-        }
-
-    }
 }
+   
+    
+
+    
+
+    // if(!phone.match(number)){
+    //     document.getElementById("number_em").style.display = "inline";
+    //     return false;
+    // }
+    // if(!mail.match(mail)){
+    //     document.getElementById("mail_em").style.display = "inline";
+    //     return false;
+    // }
+    // if(!postal.match(addres)){
+    //     document.getElementById("address_em").style.display = "inline";
+    //     return false;
+    // }
+
+    // else{
+        
+        
+        
+    //     if(phone.match(number)){
+    //         document.getElementById("number_em").style.display = "none";
+    //         return true;
+    //     }
+    //     if(mail.match(email)){
+    //         document.getElementById("mail_em").style.display = "none";
+    //         return true;
+    //     }
+    //     if(postal.match(address)){
+    //         document.getElementById("address_em").style.display = "none";
+    //         return true;
+    //     }
+
+    // }
