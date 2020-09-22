9f7256f090a3ee1ae9dbb25f45685760a384952c
Aileen Akpalu <adzo.lisa@gmail.com>
2020-09-22 23:06:46 +0000

    update

--- info.js
+++ info.js
@@ -26,7 +26,7 @@
 
 
     function checkFName(){
-        let name_len=/^[a-zA-Z\s-]+$/;
+        let name_len=/^[a-zA-Z\s-']+$/;
         let fname= document.getElementById("f_name");
         if(!fname.value.match(name_len)){
             document.getElementById("First_em").style.display = "inline";
@@ -34,7 +34,7 @@
         } 
     }
     function BlurFName(){   
-        let name_len=/^[a-zA-Z\s-]+$/;
+        let name_len=/^[a-zA-Z\s-']+$/;
         let fname= document.getElementById("f_name");
 
         if(fname.value.match(name_len)){
@@ -45,7 +45,7 @@
 }
 
     function checkMName(){
-        let name_len=/^[a-zA-Z\s-]+$/;
+        let name_len=/^[a-zA-Z\s-']+$/;
         let mname= document.getElementById("m_name");
             if(!mname.value.match(name_len)){
                 document.getElementById("Middle_em").style.display = "inline";
@@ -53,7 +53,7 @@
             }
         }
         function BlurMName(){
-            let name_len=/^[a-zA-Z\s-]+$/;
+            let name_len=/^[a-zA-Z\s-']+$/;
             let mname= document.getElementById("m_name");
                 if(mname.value.match(name_len)){
                     document.getElementById("Middle_em").style.display = "none";
@@ -62,20 +62,18 @@
         }
 
     function checkLName(){
-        let name_len=/^[a-zA-Z\s-]+$/;
+        let name_len=/^[a-zA-Z\s-']+$/;
         let lname= document.getElementById("l_name");
         if(!lname.value.match(name_len)){
-            lname.className=lname.className + "field-red";
             document.getElementById("Surname_em").style.display = "inline";
             return;
         }
     }
 
     function BlurLName(){
-        let name_len=/^[a-zA-Z\s-]+$/;
+        let name_len=/^[a-zA-Z\s-']+$/;
         let lname= document.getElementById("l_name");
         if(lname.value.match(name_len)){
-            lname.className=lname.className.replace("field-red","");
             document.getElementById("Surname_em").style.display = "none";
             return;     
                 }
@@ -84,7 +82,7 @@
 
 
     function CheckAddress(){
-        let addr_len=/^[0-9a-zA-Z]+$/;
+        let addr_len=/^[0-9a-zA-Z,.\s]+$/;
         let address=document.getElementById("myadd");
         if(!address.value.match(addr_len)){
             document.getElementById("address_em").style.display = "inline";
