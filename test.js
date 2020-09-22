1c076ad152b300e1f9a3046de9526dc888292232
melwinkle <50456098+melwinkle@users.noreply.github.com>
2020-09-22 01:13:02 +0000

    updated

--- test.js
+++ test.js
@@ -1,10 +1,6 @@
-
-    function handleSubmit(){
-        passvalue();
-    }
-
-
-
+/*
+Function to check if the number inputted is valid when the input is focused
+*/
     function checkNumber(){
         let num_len=/^[0-9]+$/;
         let phonenum=document.getElementById("mynum");
@@ -13,6 +9,10 @@
             return true;
         }
     };
+
+/*
+Function to check if the number inputted is valid when the input is blurred
+*/
 
         function BlurPhone(){
             let num_len=/^[0-9]+$/;
@@ -23,6 +23,9 @@
         }
     };
 
+/*
+Function to check if the name inputted is valid when the input is focused
+*/
 
     function checkFName(){
         let name_len=/^[A-Za-z\s]+$/;
@@ -32,6 +35,10 @@
             return true;
         } 
     };
+
+/*
+Function to check if the name inputted is valid when the input is blurred
+*/
     function BlurFName(){   
         let name_len=/^[A-Za-z\s]+$/;
         let fname= document.getElementById("f_name");
@@ -43,14 +50,22 @@
     
 };
 
-    function checkMName(){
-        let name_len=/^[A-Za-z\s]+$/;
-        let mname= document.getElementById("m_name");
-            if(!mname.value.match(name_len)){
-                document.getElementById("Middle_em").style.display = "inline";
-                return true;
-            }
-        };
+/*
+Function to check if the name inputted is valid when the input is focused
+*/
+
+function checkMName(){
+    let name_len=/^[A-Za-z\s]+$/;
+    let mname= document.getElementById("m_name");
+    if(!mname.value.match(name_len)){
+        document.getElementById("Middle_em").style.display = "inline";
+    return true;
+    }
+};
+
+/*
+Function to check if the name inputted is valid when the input is blurred
+*/
         function BlurMName(){
             let name_len=/^[A-Za-z\s\s]+$/;
             let mname= document.getElementById("m_name");
@@ -59,7 +74,9 @@
                 return false;
             }
         };
-
+/*
+Function to check if the name inputted is valid when the input is focused
+*/
     function checkLName(){
         let name_len=/^[A-Za-z\s]+$/;
         let lname= document.getElementById("l_name");
@@ -69,6 +86,10 @@
             return;
         }
     };
+
+/*
+Function to check if the name inputted is valid when the input is blurred
+*/
 
     function BlurLName(){
         let name_len=/^[A-Za-z\s]+$/;
@@ -80,7 +101,9 @@
                 }
                
     };
-
+/*
+Function to check if the address inputted is valid when the input is focused
+*/
 
     function CheckAddress(){
         let addr_len=/^[0-9a-zA-Z\s]+$/;
@@ -90,7 +113,9 @@
             return true;
         }
     };
-
+/*
+Function to check if the address inputted is valid when the input is blurred
+*/
     function BlurAddress(){
         let addr_len=/^[0-9a-zA-Z\s]+$/;
         let address=document.getElementById("myadd");
@@ -100,7 +125,9 @@
           }
         };
    
-
+/*
+Function to check if the email inputted is valid when the input is focused
+*/
     function checkEmail(){
         let mail_len=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
         let mail=document.getElementById("mymail");
@@ -109,6 +136,10 @@
             return true;
         }
     };
+
+/*
+Function to check if the email inputted is valid when the input is blurred
+*/
         function BlurMail(){
             let mail=document.getElementById("mymail");
             let mail_len=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -120,21 +151,9 @@
     }
 
 
-    // function checkGender(){
-    //     let gender=document.getElementById("gender");
-    //     if(gender.value=="Select an Option"){
-    //         document.getElementById("gender_em").style.display = "inline";
-    //         return false;
-    //     }
-    // };
-
-    // function BlurGender(){
-    //     let gender=document.getElementById("gender");
-    //     if((gender.value=="Male")||(gender.value=="Female")){
-    //         document.getElementById("gender_em").style.display = "none";
-    //         return true;
-    //         }
-    // };
+    /*
+Function to set the values in the form and store them locally
+*/
 
    
     function passvalue(){
