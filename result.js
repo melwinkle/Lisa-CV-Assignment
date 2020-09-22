d9fee241c21b7ae2acd6a0531d7eab6bb638ed62
melwinkle <50456098+melwinkle@users.noreply.github.com>
2020-09-22 01:15:16 +0000

    updated

--- result.js
+++ result.js
@@ -1,8 +1,8 @@
 window.addEventListener('load',()=>{
 
-    document.getElementById('fame').innerHTML=localStorage.getItem("first")+localStorage.getItem("middle")+localStorage.getItem("last");
-    // document.getElementById('mame').innerHTML=localStorage.getItem("middle");
-    // document.getElementById('lame').innerHTML=localStorage.getItem("last");
+    document.getElementById('fame').innerHTML=localStorage.getItem("first");
+    document.getElementById('mame').innerHTML=localStorage.getItem("middle");
+    document.getElementById('lame').innerHTML=localStorage.getItem("last");
     document.getElementById('address-res').innerHTML=localStorage.getItem("address");
     document.getElementById('gender-res').innerHTML=localStorage.getItem("gender");
     document.getElementById('number-res').innerHTML=localStorage.getItem("number");
